d8648aaa97e6cbd063400b606585de61dfd26519
maikalangi <maikalangi@gmail.com>
2022-05-06 07:10:41 -0700

    adds new remove function inside new skill list and adjusts css list and button styles

--- js/script.js
+++ js/script.js
@@ -1,41 +1,26 @@
-// // the "document ready" approach
-// $(document).ready(function () {
-//     // all of your app's js goes within this function
-//     alert("Everything is ready, let's do this")
-//   })
-  
-//   // or, the shortcut version
-//   $(function () {
-//     // all of your app's js goes within this function
-//     alert("Everything is ready, let's do this")
-//   })
-
 const $ul = $('.list')
 const $button = $('#button')
 const $input = $('#field')
 
 const addSkill = $input.val()
 
-console.log(addSkill)
-
 // function will add skill from input field into newly created list under title
 
 $button.click(()=>{
-    // while ($input.val()!==''){
-    const $skill = $('<li class="skill"></li>');
-    const $close = $('<input class="close" type="button" value="X"/>')
+    const $skill = $('<li></li>');
+    const $close = $('<button class="close">X</button>');
+    $close.css({'height': 'auto','width': 'auto'});
     $skill.css('list-style-type', 'none');
     $skill.text($input.val());
     $ul.append($skill);
     $ul.append($close);
-    // }
+
+    // new list will have button with function to clear skill entry
+
+    $close.click(()=>{
+        $skill
+        .remove();
+        $close.remove();
+    });
     $input.val('');
-});
-
-// new list will have button with function to clear skill entry
-
-// const $close = $('.close')
-// const $skill = $('.skill')
-$('input.close').click(()=>{
-    $('li.skill').remove();
 });
